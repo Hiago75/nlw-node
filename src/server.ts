ebf7427eda426e3a0fb9fa384372ae79f236cf47
Hiago75 <hiagobrenha@protonmail.com>
2021-06-21 22:32:23 -0300

    DB Added. CreateUserMigration added. Entity User added. UserRepositories added. CreateUserService added. CreateUserController added.

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
+import "reflect-metadata";
 import express from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
 
-app.get('/test', (request, response) => {
-  return response.send("Olá NLW")
-})
-
-app.post("/test-post", (request, response) => {
-  return response.send('Olá NLW método Post')
-})
-
+app.use(express.json());
+app.use(router);
 
 /* É responsável por colocar nosso servidor no ar, recebe 2 argumentos, sendo o primeiro a porta em que o server vai ser hospedado e o segundo uma função callback que será executada assim que o servidor for iniciado*/
-app.listen(3000, () => console.log('Server is running')) 
+app.listen(3000, () => console.log('Server is running'))
